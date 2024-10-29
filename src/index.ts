73cd3b5c9e87cf17a47af9776b2217ed2b27cc0e
Oliver Mak <oliver.mak@naxos.com>
2024-10-29 11:57:04 +0800

    Main

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("asd");
+#!/usr/bin/env node
+import { execSync } from "child_process";
+
+execSync(`pnpm list -g -P --no-optional`, { stdio: "inherit" });
+
+console.log("\nUpdating...");
+const output = JSON.parse(execSync(`pnpm list -g --json`).toString()) as Record<string, unknown>[];
+const deps = Object.keys(output[0].dependencies as Record<string, Record<string, string>>);
+execSync(`pnpm add -g ${deps.join(" ")}`);
+
+console.log("\nDone");
