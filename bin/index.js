69e5214cfb4d82a32c6885525bd8f8eb2d63ec9a
Oliver Mak <oliver.mak@naxos.com>
2024-11-15 16:41:22 +0800

    Try to show more log

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,7 @@
-"use strict";var e=require("child_process");e.execSync("pnpm list -g -P --no-optional",{stdio:"inherit"}),console.log(`
-Updating...`);const n=JSON.parse(e.execSync("pnpm list -g --json").toString())[0],o=Object.keys(n.dependencies);e.execSync(`pnpm add -g ${o.join(" ")}`),console.log(`
-Done`);
+#!/usr/bin/env node
+"use strict";var o=require("child_process");o.execSync("pnpm list -g -P --no-optional",{stdio:"inherit"}),console.log(`
+=========================================`),console.log("Command to be executed..."),console.log(`=========================================
+`);const n=JSON.parse(o.execSync("pnpm list -g --json").toString()),e=Object.keys(n[0].dependencies);console.log(`pnpm add -g ${e.join(" ")}`),console.log(`
+=========================================`),console.log("Updating..."),console.log(`=========================================
+`),o.execSync(`pnpm add -g ${e.join(" ")}`,{stdio:"inherit"}),console.log(`
+=========================================`),console.log("Done"),console.log("=========================================");
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,20 @@ import { execSync } from "child_process";
 
 execSync(`pnpm list -g -P --no-optional`, { stdio: "inherit" });
 
-console.log("\nUpdating...");
+console.log("\n=========================================");
+console.log("Command to be executed...");
+console.log("=========================================\n");
+
 const output = JSON.parse(execSync(`pnpm list -g --json`).toString()) as Record<string, unknown>[];
 const deps = Object.keys(output[0].dependencies as Record<string, Record<string, string>>);
-execSync(`pnpm add -g ${deps.join(" ")}`);
+console.log(`pnpm add -g ${deps.join(" ")}`);
 
-console.log("\nDone");
+console.log("\n=========================================");
+console.log("Updating...");
+console.log("=========================================\n");
+
+execSync(`pnpm add -g ${deps.join(" ")}`, { stdio: "inherit" });
+
+console.log("\n=========================================");
+console.log("Done");
+console.log("=========================================");
