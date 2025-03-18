78a90912d97f8e44012fb2ae3f44f1ba9bfbf8fc
Oliver Mak <oliver.mak@naxos.com>
2025-03-18 10:39:36 +0800

    Update dependcies and eslint

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,23 +1,23 @@
-import del from "rollup-plugin-delete";
-import esbuild from "rollup-plugin-esbuild";
+import del from 'rollup-plugin-delete'
+import esbuild from 'rollup-plugin-esbuild'
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
-        dir: "bin",
-        format: "cjs",
+        dir: 'bin',
+        format: 'cjs',
         sourcemap: false,
       },
     ],
-    external: ["child_process"],
+    external: ['child_process'],
     plugins: [
-      del({ targets: "bin/*" }),
+      del({ targets: 'bin/*' }),
       esbuild({
         minify: true,
-        drop: ["debugger"],
+        drop: ['debugger'],
       }),
     ],
   },
-];
+]
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
-import { execSync } from "child_process";
+import { execSync } from 'node:child_process'
 
-execSync(`pnpm list -g -P --no-optional`, { stdio: "inherit" });
+execSync(`pnpm list -g -P --no-optional`, { stdio: 'inherit' })
 
-console.log("\n=========================================");
-console.log("Command to be executed...");
-console.log("=========================================\n");
+console.log('\n=========================================')
+console.log('Command to be executed...')
+console.log('=========================================\n')
 
-const output = JSON.parse(execSync(`pnpm list -g --json`).toString()) as Record<string, unknown>[];
-const deps = Object.keys(output[0].dependencies as Record<string, Record<string, string>>);
-console.log(`pnpm self-update`);
-console.log(`pnpm add -g ${deps.join(" ")}`);
+const output = JSON.parse(execSync(`pnpm list -g --json`).toString()) as Record<string, unknown>[]
+const deps = Object.keys(output[0].dependencies as Record<string, Record<string, string>>)
+console.log(`pnpm self-update`)
+console.log(`pnpm add -g ${deps.join(' ')}`)
 
-console.log("\n=========================================");
-console.log("Updating...");
-console.log("=========================================\n");
+console.log('\n=========================================')
+console.log('Updating...')
+console.log('=========================================\n')
 
-execSync(`pnpm self-update`);
-execSync(`pnpm add -g ${deps.join(" ")}`, { stdio: "inherit" });
+execSync(`pnpm self-update`)
+execSync(`pnpm add -g ${deps.join(' ')}`, { stdio: 'inherit' })
 
-console.log("\n=========================================");
-console.log("Done");
-console.log("=========================================");
+console.log('\n=========================================')
+console.log('Done')
+console.log('=========================================')
