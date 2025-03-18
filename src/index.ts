fa546e516c9e3c915e284958564462cc0b987e47
Oliver Mak <oliver.mak@naxos.com>
2025-03-18 10:19:38 +0800

    Add script for `pnpm self-update`

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,14 @@ console.log("=========================================\n");
 
 const output = JSON.parse(execSync(`pnpm list -g --json`).toString()) as Record<string, unknown>[];
 const deps = Object.keys(output[0].dependencies as Record<string, Record<string, string>>);
+console.log(`pnpm self-update`);
 console.log(`pnpm add -g ${deps.join(" ")}`);
 
 console.log("\n=========================================");
 console.log("Updating...");
 console.log("=========================================\n");
 
+execSync(`pnpm self-update`);
 execSync(`pnpm add -g ${deps.join(" ")}`, { stdio: "inherit" });
 
 console.log("\n=========================================");
