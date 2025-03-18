8179d0a59d5676c062be5617085328e18fd929cf
Oliver Mak <oliver.mak@naxos.com>
2025-03-18 11:04:36 +0800

    Update bin

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-"use strict";var o=require("child_process");o.execSync("pnpm list -g -P --no-optional",{stdio:"inherit"}),console.log(`
+"use strict";var o=require("node:child_process");o.execSync("pnpm list -g -P --no-optional",{stdio:"inherit"}),console.log(`
 =========================================`),console.log("Command to be executed..."),console.log(`=========================================
-`);const n=JSON.parse(o.execSync("pnpm list -g --json").toString()),e=Object.keys(n[0].dependencies);console.log(`pnpm add -g ${e.join(" ")}`),console.log(`
+`);const n=JSON.parse(o.execSync("pnpm list -g --json").toString()),e=Object.keys(n[0].dependencies);console.log("pnpm self-update"),console.log(`pnpm add -g ${e.join(" ")}`),console.log(`
 =========================================`),console.log("Updating..."),console.log(`=========================================
-`),o.execSync(`pnpm add -g ${e.join(" ")}`,{stdio:"inherit"}),console.log(`
+`),o.execSync("pnpm self-update"),o.execSync(`pnpm add -g ${e.join(" ")}`,{stdio:"inherit"}),console.log(`
 =========================================`),console.log("Done"),console.log("=========================================");
